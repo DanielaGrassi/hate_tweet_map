631a1286165948157047a44bbd6046253463681e
Daniela Grassi <daniela.grassi.98@gmail.com>
2022-01-14 23:32:12 +0100

    add tweets to data.json, add the rules of schwa and articles, add a dataset of tweets for labelling

--- script/inclusivity_management/Rules.py
+++ script/inclusivity_management/Rules.py
@@ -6,12 +6,11 @@ import pandas as pd
 
 
 def read_tsv(tsv):
-
     rd = pd.read_csv(tsv,sep='\t')
     return rd
 
-def check_female_name(token):
 
+def check_female_name(token):
     with open('docs/nomif.txt', 'r', encoding='utf-8') as f:
         myNamesFemale = set(line.strip() for line in f)
 
@@ -19,6 +18,7 @@ def check_female_name(token):
         return True
     else:
         return False
+
 
 def check_male_name(token):
 
@@ -29,6 +29,7 @@ def check_male_name(token):
         return True
     else:
         return False
+
 
 def check_surname(token):
 
@@ -41,8 +42,7 @@ def check_surname(token):
         return False
 
 
-
-def article_noun(tweet):
+def article_noun(tweet, explain):
 # Articolo femminile + nome proprio (spesso si riferisce solo alle donne)
     inclusive = 0.0
     for idx, (token, tag, det, morph) in enumerate(tweet):
@@ -57,7 +57,7 @@ def article_noun(tweet):
     return inclusive
 
 
-def femaleName_maleAppos(tweet, male_crafts):
+def femaleName_maleAppos(tweet, male_crafts, explain):
 # Alessia è un avvocato formidabile
     inclusive = 0.0
     for idx, (token, tag, det, morph) in enumerate(tweet):
@@ -70,7 +70,7 @@ def femaleName_maleAppos(tweet, male_crafts):
 
     return inclusive
 
-def art_donna_noun(tweet, crafts):
+def art_donna_noun(tweet, crafts, explain):
     inclusive= 0.0
     for idx, (token, tag, det, morph) in enumerate(tweet):
         if token == 'donna' and idx != 0:
@@ -78,11 +78,12 @@ def art_donna_noun(tweet, crafts):
                 if tweet[idx+1][1] == 'NOUN' and tweet[idx+1][2] == 'compound':
                     if tweet[idx+1][0] in crafts:
                         inclusive = - 0.25
-                        print("Utilizzare il sostantivo 'donna' con un' apposizione maschile' diminuisce l'inclusività")
+                        if explain:
+                            print("Utilizzare il sostantivo 'donna' con un' apposizione maschile' diminuisce l'inclusività")
     return inclusive
 
 
-def maleAppos_femaleName(tweet, male_crafts):
+def maleAppos_femaleName(tweet, male_crafts, explain):
     # L'assessore Alessia
     inclusive = 0.0
     for idx, (token, tag, det, morph) in enumerate(tweet):
@@ -90,10 +91,11 @@ def maleAppos_femaleName(tweet, male_crafts):
             if (idx + 1 < len(tweet)):
                 if tweet[idx + 1][1] == 'PROPN' and check_female_name(tweet[idx + 1][0]):
                     inclusive = - 0.25
-                    print("Utilizzare un'apposizione  maschile con un nome femminile diminuisce l'inclusività")
+                    if explain:
+                        print("Utilizzare un'apposizione  maschile con un nome femminile diminuisce l'inclusività")
     return inclusive
 
-def noun_donna(tweet, male_crafts):
+def noun_donna(tweet, male_crafts, explain):
     inclusive= 0.0
     for idx, (token, tag, det, morph) in enumerate(tweet):
         if (idx + 1 < len(tweet)):
@@ -101,11 +103,12 @@ def noun_donna(tweet, male_crafts):
                 if token == 'donna' and idx != 0:
                   if tweet[idx+1][1] == 'NOUN' and tweet[idx+1][2] == 'compound':
                      inclusive = - 0.25
-                     print("Utilizzare un'apposizione maschile con il sostantivo 'donna' diminuisce l'inclusività")
+                     if explain:
+                         print("Utilizzare un'apposizione maschile con il sostantivo 'donna' diminuisce l'inclusività")
     return inclusive
 
 
-def femaleSub_malePart(tweet):
+def femaleSub_malePart(tweet, explain):
     inclusive= 0.0
     for idx, (token, tag, det, morph) in enumerate(tweet):
         if tag == 'PROPN' or tag == 'NOUN' and det == 'nsubj':
@@ -118,11 +121,12 @@ def femaleSub_malePart(tweet):
                             if 'Gender' in morph and 'VerbForm' in morph and 'Number' in morph:
                                 if tweet[idx+1][3]['Gender'] == 'Masc' and tweet[idx+1][3]['VerbForm'] == 'Part' and tweet[idx+1][3]['Number'] == 'Sing':
                                     inclusive = - 0.25
-                                    print("Utilizzare un sostantivo femminile con un verbo al maschile diminuisce l'inclusività")
+                                    if explain:
+                                        print("Utilizzare un sostantivo femminile con un verbo al maschile diminuisce l'inclusività")
     return inclusive
 
 
-def article_inclusive(tweet):
+def pronoun_inclusive(tweet, explain):
     inclusive = 0.0
     for idx, (token, tag, det, morph) in enumerate(tweet):
         if tag == 'PRON' or tag == 'NOUN':
@@ -132,41 +136,71 @@ def article_inclusive(tweet):
                        if tweet[idx + 1][0] == '/' or tweet[idx + 1][0] == '\\':
                           if tweet[idx + 2][1] == 'PRON' or tweet[idx + 2][1] == 'NOUN' and morph[idx + 2]['Gender'] == 'Fem':
                              inclusive = 0.25
-                             print("Utilizzare gli articoli declinati in più forme aumenta l'inclusività")
+                             if explain:
+                                 print("Utilizzare i pronomi declinati in più forme aumenta l'inclusività")
     return inclusive
 
+#TODO Complete this function
+def article_inclusive(tweet, explain):
+    inclusive = 0.0
+    for idx, (token, tag, det, morph) in enumerate(tweet):
+        #print(token, tag, det, morph)
+        if tag == 'PRON' or tag == 'NOUN':
+            #print(tag, det, morph, token)
+            if 'Gender' in morph:
+               if morph['Gender'] == 'Masc':
+                   if (idx+2 < len(tweet)):
+                       if tweet[idx + 1][0] == '/' or tweet[idx + 1][0] == '\\':
+                          if tweet[idx + 2][1] == 'PRON' or tweet[idx + 2][1] == 'NOUN' and morph[idx + 2]['Gender'] == 'Fem':
+                             inclusive = 0.25
+                             if explain:
+                                 print("Utilizzare gli articoli declinati in più forme aumenta l'inclusività")
+    return inclusive
 
-def male_collettives(tweet, male_crafts):
+def schwa(tweet, explain):
+    inclusive = 0.0
+    for idx, (token, tag, det, morph) in enumerate(tweet):
+        if token.endswith('*') or token.endswith('ə'):
+            inclusive = 0.25
+            if explain:
+              print("Utilizzare caratteri come la schwa o l'asterisco alla fine di una parola aumenta l'inclusività ")
+    return inclusive
+
+def male_collettives(tweet, male_crafts, explain):
     inclusive = 0.0
     # Se ho che il nome che ho nella frase è un mestiere maschile
     for idx, (token, tag, det, morph) in enumerate(tweet):
-        doc=nlp(token)
+        doc = nlp(token)
         for w in doc:
             if tag == 'NOUN' and w.lemma_ in male_crafts:
                 if 'Number' in morph and morph['Number'] == 'Plur':
                     counter_propn=0
                     for words in tweet:
                         token_words, tag_words, det_words, morph_words= words
-                        if tag_words=="PROPN":
+                        if tag_words == "PROPN":
                             if check_male_name(token_words): # Controllare se è un nome proprio maschile (da una lista)
-                                counter_propn= counter_propn+1
+                                counter_propn = counter_propn+1
                     if counter_propn > 1:
                         inclusive=inclusive
                     else:
                         inclusive= -0.25
-                        print("Utilizzare un nome collettivo maschile diminuisce l'inclusività!")
+                        if explain:
+                            print("Utilizzare un nome collettivo maschile diminuisce l'inclusività!")
     return inclusive
 
-def male_expressions(sentence):
+def male_expressions(sentence, explain):
     with open('docs/uomini_di.txt', 'r', encoding='utf-8') as f:
         myExpressions = set(line.strip() for line in f)
     inclusive = 0.0
     for expression in myExpressions:
+        #TODO Aggiungere controllo su "desiderio di paternità"
         if str(expression).lower() in str(sentence).lower():
             inclusive = - 0.25
+            if explain:
+                print("stringa che fa l'explain di questa regola")
     return inclusive
 
-def male_female_jobs(tweet, male_list, female_list):
+def male_female_jobs(tweet, male_list, female_list, explain):
     inclusive= 0.0
     for idx, (token, tag, det, morph) in enumerate(tweet):
         doc = nlp(token)
@@ -182,34 +216,44 @@ def male_female_jobs(tweet, male_list, female_list):
                                 pos1=female_list.index(d.lemma_)
                                 if pos == pos1:
                                     if 'Number' in morph_words and morph_words['Number'] == 'Plur':
-                                        inclusive = 0.25
-                                        print("Utilizzare sia il femminile che il maschile per esprimere i lavori aumenta l'inclusività")
+                                        inclusive = 0.50
+                                        if explain:
+                                            print("Utilizzare sia il femminile che il maschile per esprimere i lavori aumenta l'inclusività")
     return inclusive
 
 if __name__ == "__main__":
+
+
     path = '../../data.json'
     nlp = spacy.load("it_core_news_lg")
     crafts_path = '../../script/inclusivity_management/docs/list.tsv'
 
     ## inclusivity for the whole dataset
     crafts = read_tsv(crafts_path)
-    male_list=list(crafts['itemLabel'])
-    female_list=list(crafts['femaleLabel'])
+    male_list = list(crafts['itemLabel'])
+    female_list = list(crafts['femaleLabel'])
     male_crafts = set(crafts['itemLabel'].unique())
     female_crafts = set(crafts['femaleLabel'].unique())
     sentences, ph = rp.runtimePos(path)
+
+    df = pd.DataFrame({'Tweet': sentences})
+    df['Tweet'].to_csv('../../tweets.csv', encoding='utf-8-sig', index = False)
+
     for sentence, phrase in zip(sentences, ph):
         inclusive = 0.0
-        inclusive += article_noun(phrase)
-        inclusive += femaleName_maleAppos(phrase, male_crafts)
-        inclusive += art_donna_noun(phrase, male_crafts)
-        inclusive += noun_donna(phrase, male_crafts)
-        inclusive += femaleSub_malePart(phrase)
-        inclusive += maleAppos_femaleName(phrase, male_crafts)
-        inclusive += article_inclusive(phrase)
-        inclusive += male_collettives(phrase, male_crafts)
-        inclusive += male_expressions(sentence)
-        inclusive += male_female_jobs(phrase, male_list, female_list)
+        explain = True
+        inclusive += schwa(phrase, explain)
+        inclusive += article_noun(phrase, explain)
+        inclusive += pronoun_inclusive(phrase, explain)
+        inclusive += femaleName_maleAppos(phrase, male_crafts, explain)
+        inclusive += art_donna_noun(phrase, male_crafts, explain)
+        inclusive += noun_donna(phrase, male_crafts, explain)
+        inclusive += femaleSub_malePart(phrase, explain)
+        inclusive += maleAppos_femaleName(phrase, male_crafts, explain)
+        inclusive += article_inclusive(phrase, explain)
+        inclusive += male_collettives(phrase, male_crafts, explain)
+        inclusive += male_expressions(sentence, explain)
+        inclusive += male_female_jobs(phrase, male_list, female_list, explain)
         print(sentence)
 
         print(inclusive)
